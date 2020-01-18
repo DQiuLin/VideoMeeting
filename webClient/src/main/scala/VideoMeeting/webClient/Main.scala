046e0434b45688d1aa3a897be884cfe194c33717
[duanqiulin] <[duanqiulin@neotel.com.cn]>
2020-01-18 19:07:29 +0800

    webClient util

--- webClient/src/main/scala/VideoMeeting/webClient/Main.scala
+++ webClient/src/main/scala/VideoMeeting/webClient/Main.scala
@@ -1,9 +1,8 @@
 package VideoMeeting.webClient
 
-import VideoMeeting.webClient.pages.MainPage
-import org.scalajs.dom
-
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+import VideoMeeting.webClient.pages.MainPage
+
 /**
   * create by zhaoyin
   * 2019/7/17  2:35 PM
@@ -17,6 +16,6 @@ object Main {
 
   @JSExport
   def run(): Unit = {
-
+    MainPage.show()
   }
 }
--- webClient/src/main/scala/VideoMeeting/webClient/pages/MainPage.scala
+++ webClient/src/main/scala/VideoMeeting/webClient/pages/MainPage.scala
@@ -2,4 +2,7 @@ package VideoMeeting.webClient.pages
 
 object MainPage {
 
+  def show():Unit={
+    <div></div>
+  }
 }

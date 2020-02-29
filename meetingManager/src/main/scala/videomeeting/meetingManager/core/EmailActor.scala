77748a405b2d3fd5f6a7339a26e0506a1bac9d85
asxdfd <16301170@bjtu.edu.cn>
2020-02-29 17:29:41 +0800

    debug

--- meetingManager/src/main/scala/videomeeting/meetingManager/core/EmailActor.scala
+++ meetingManager/src/main/scala/videomeeting/meetingManager/core/EmailActor.scala
@@ -77,6 +77,7 @@ object EmailActor {
     p.put("mail.transport.protocol", "smtp")
     p.put("mail.smtp.auth", "true")
     p.put("mail.stmp.ssl.enable", "true")
+    p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
     p
   }
 

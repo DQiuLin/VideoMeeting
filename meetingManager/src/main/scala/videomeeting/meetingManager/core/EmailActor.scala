80246bfe3902a6d9682eb5400f2e5d603c473e54
asxdfd <16301170@bjtu.edu.cn>
2020-02-29 17:50:12 +0800

    debug

--- meetingManager/src/main/scala/videomeeting/meetingManager/core/EmailActor.scala
+++ meetingManager/src/main/scala/videomeeting/meetingManager/core/EmailActor.scala
@@ -61,6 +61,7 @@ object EmailActor {
           mainPart.addBodyPart(html)
           message.setContent(mainPart)
           Transport.send(message)
+          log.info(s"Send email succeed.")
           Behaviors.same
 
         case x =>
@@ -76,7 +77,7 @@ object EmailActor {
     p.put("mail.smtp.port", AppSettings.emailPort)
     p.put("mail.transport.protocol", "smtp")
     p.put("mail.smtp.auth", "true")
-    p.put("mail.stmp.ssl.enable", "true")
+//    p.put("mail.stmp.ssl.enable", "true")
 //    p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
     p
   }
